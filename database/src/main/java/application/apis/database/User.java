d96bf3667cc4fa7a8eb169a11314a842f9d2da48
anandsaw <anandsaw@W5CG9352VM9.ap.tieto.com>
2024-10-24 16:04:55 +0530

    database

---
+++ database/src/main/java/application/apis/database/User.java
@@ -0,0 +1,87 @@
+package application.apis.database;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
+//import javax.validation.constraints.Email;
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty(message = "Username is mandatory")
+    private String username;
+
+    @NotEmpty(message = "Password is mandatory")
+    private String password;
+//    @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @NotEmpty(message = "Email cannot be empty")
+    private String email;
+
+    @NotEmpty(message = "First name is mandatory")
+    private String firstname;
+
+    @NotEmpty(message = "Last name is mandatory")
+    private String lastname;
+
+    @NotEmpty(message = "Mobile number is mandatory")
+    @Pattern(regexp = "^\\d{10}$",message="invalid mob no.")
+    private String mobileno;
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public String getFirstName() {
+		return firstname;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstname = firstName;
+	}
+
+	public String getLastName() {
+		return lastname;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastname = lastName;
+	}
+
+	public String getMobileNo() {
+		return mobileno;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileno = mobileNo;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+    // Getters and Setters	
+	
+}
